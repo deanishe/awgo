36ac92a12b44cd5dba47ce44716d2b910d562efe
Dean Jackson <deanishe@deanishe.net>
2016-05-24 01:14:42 +0200

    Add padding tests

--- util_test.go
+++ util_test.go
@@ -8,7 +8,76 @@
 
 package workflow
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
+
+type padTest struct {
+	str      string
+	pad      string
+	n        int
+	expected string
+}
+
+var padLeftTests = []padTest{
+	// Simple cases
+	padTest{"wow", "-", 5, "--wow"},
+	padTest{"pow", " ", 4, " pow"},
+	// Input same length as n
+	padTest{"pow", " ", 3, "pow"},
+	// Input longer than n
+	padTest{"powwow", " ", 3, "powwow"},
+}
+
+var padRightTests = []padTest{
+	// Simple cases
+	padTest{"wow", "-", 5, "wow--"},
+	padTest{"pow", " ", 4, "pow "},
+	// Input same length as n
+	padTest{"pow", " ", 3, "pow"},
+	// Input longer than n
+	padTest{"powwow", " ", 3, "powwow"},
+}
+
+var padTests = []padTest{
+	// Simple cases
+	padTest{"wow", "-", 5, "-wow-"},
+	padTest{"pow", " ", 4, "pow "},
+	// Even-length str
+	padTest{"wow", "-", 10, "---wow----"},
+	// Input same length as n
+	padTest{"pow", " ", 3, "pow"},
+	// Input longer than n
+	padTest{"powwow", " ", 3, "powwow"},
+}
+
+// TestPadLeft tests PadLeft
+func TestPadLeft(t *testing.T) {
+	for _, td := range padLeftTests {
+		if out := PadLeft(td.str, td.pad, td.n); out != td.expected {
+			t.Fatalf("PadLeft output incorrect. Expected=%v, Got=%v", td.expected, out)
+		}
+	}
+}
+
+// TestPadRight tests PadRight
+func TestPadRight(t *testing.T) {
+	for _, td := range padRightTests {
+		if out := PadRight(td.str, td.pad, td.n); out != td.expected {
+			t.Fatalf("PadRight output incorrect. Expected=%v, Got=%v", td.expected, out)
+		}
+	}
+}
+
+// TestPad tests Pad
+func TestPad(t *testing.T) {
+	for _, td := range padTests {
+		if out := Pad(td.str, td.pad, td.n); out != td.expected {
+			t.Fatalf("Pad output incorrect. Expected=%v, Got=%v", td.expected, out)
+		}
+	}
+}
 
 func ExamplePadLeft() {
 	fmt.Println(PadLeft("wow", "-", 5))
@@ -21,11 +90,6 @@ func ExamplePadRight() {
 }
 
 func ExamplePad() {
-	fmt.Println(Pad("wow", "-", 5))
-	// Output: -wow-
-}
-
-func ExamplePad_longer() {
 	fmt.Println(Pad("wow", "-", 10))
 	// Output: ---wow----
 }
