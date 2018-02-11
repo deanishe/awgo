44682a187e6bc998a94fef56a2ce9bce01539391
Dean Jackson <deanishe@deanishe.net>
2018-02-11 18:05:13 +0100

    Add Script Running

--- util/examples_test.go
+++ util/examples_test.go
@@ -12,10 +12,12 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"time"
+	"strings"
 )
 
+// Shorten paths by replacing user's home directory with ~
 func ExamplePrettyPath() {
+
 	paths := []string{
 		"",
 		"$HOME",
@@ -53,29 +55,6 @@ func ExamplePad() {
 	// Output: ---wow----
 }
 
-func ExampleReadableDuration() {
-	fmt.Println(HumanDuration(time.Hour * 96))
-	fmt.Println(HumanDuration(time.Hour * 48))
-	fmt.Println(HumanDuration(time.Hour * 12))
-	fmt.Println(HumanDuration(time.Minute * 130))
-	fmt.Println(HumanDuration(time.Minute * 90))
-	fmt.Println(HumanDuration(time.Second * 315))
-	fmt.Println(HumanDuration(time.Second * 70))
-	fmt.Println(HumanDuration(time.Second * 5))
-	fmt.Println(HumanDuration(time.Millisecond * 320))
-	fmt.Println(HumanDuration(time.Millisecond * 50))
-	// Output: 4d
-	// 48h
-	// 12h0m
-	// 2h10m
-	// 90m
-	// 5m15s
-	// 70s
-	// 5.0s
-	// 0.32s
-	// 50ms
-}
-
 func ExamplePathExists() {
 
 	name := "my-test-file.txt"
@@ -97,6 +76,7 @@ func ExamplePathExists() {
 	}
 }
 
+// QuoteAS wraps the string in quotes and escapes quotes within the string.
 func ExampleQuoteAS() {
 	values := []string{
 		"",
@@ -118,4 +98,167 @@ func ExampleQuoteAS() {
 	// "has " & quote & "quotes" & quote & " within"
 	// quote & "within quotes" & quote
 	// quote
+}
+
+func ExampleRunAS() {
+
+	// Some test words
+	data := []string{
+		"Hello, AppleScript!",
+		`"Just Do It!"`,
+		`He said, "I'm fine!" then died :(`,
+		`"`,
+	}
+
+	for _, input := range data {
+
+		// Simple script to return input
+		// QuoteAS adds quotation marks, so don't add any more
+		quoted := QuoteAS(input)
+		script := "return " + quoted
+
+		// Run script and collect result
+		output, err := RunAS(script)
+		if err != nil {
+			// handle error
+		}
+
+		fmt.Printf("> %s\n", input)
+		fmt.Printf("< %s\n", output)
+	}
+
+	// Output:
+	// > Hello, AppleScript!
+	// < Hello, AppleScript!
+	// > "Just Do It!"
+	// < "Just Do It!"
+	// > He said, "I'm fine!" then died :(
+	// < He said, "I'm fine!" then died :(
+	// > "
+	// < "
+
+}
+
+// You can pass additional arguments to your scripts.
+func ExampleRunJS_arguments() {
+
+	// Some test values
+	argv := []string{"angular", "react", "vue"}
+
+	script := `function run(argv) { return argv.join('\n') }`
+	output, err := RunJS(script, argv...)
+	if err != nil {
+		// handle error
+	}
+
+	fmt.Println(output)
+
+	// Output:
+	// angular
+	// react
+	// vue
+}
+
+// Run calls any executable file. It does *not* use $PATH to find commands.
+func ExampleRun() {
+
+	// Create a simple test script
+	filename := "test-script"
+	script := "#!/bin/bash\necho -n Happy Hour"
+
+	// Make sure script is executable!
+	if err := ioutil.WriteFile(filename, []byte(script), 0700); err != nil {
+		panic(err)
+	}
+
+	// Note: we're running "test-script", but Run looks for "./test-script",
+	// not a command "test-script" on your $PATH.
+	out, err := Run(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(out))
+
+	// Output:
+	// Happy Hour
+
+	if err := os.Remove(filename); err != nil {
+		panic(err)
+	}
+}
+
+// You can pass arguments to the program/script you run.
+func ExampleRun_arguments() {
+
+	// Run an executable with arguments
+	out, err := Run("/bin/bash", "-c", "echo -n Stringfellow Hawke")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(out))
+
+	// Output:
+	// Stringfellow Hawke
+}
+
+// Run recognises certain kinds of script files and knows which
+// interpreter to run them with.
+func ExampleRun_scripts() {
+
+	// Test scripts that output $1.
+	// Run will run them based on their file extension.
+	scripts := []struct {
+		name, code string
+	}{
+		{"test-file.py", "import sys; print(sys.argv[1])"},
+		{"test-file.txt", "ignored"}, // invalid
+		{"test-file.sh", `echo "$1"`},
+		{"test-file.scpt", "on run(argv)\nreturn first item of argv\nend run"},
+		{"test-file.doc", "irrelevant"}, // invalid
+	}
+
+	// Create test scripts. Note: they aren't executable.
+	for _, script := range scripts {
+		if err := ioutil.WriteFile(script.name, []byte(script.code), 0600); err != nil {
+			panic(err)
+		}
+	}
+
+	// Run scripts
+	for _, script := range scripts {
+
+		// Run runs file based on file extension
+		// Pass script's own name as $1
+		data, err := Run(script.name, script.name)
+		if err != nil {
+
+			// We're expecting 2 unknown types
+			if err == ErrUnknownFileType {
+				fmt.Printf("[err] %s: %s\n", err, script.name)
+				continue
+			}
+
+			// Oops :(
+			panic(err)
+		}
+
+		// Script's own name
+		str := strings.TrimSpace(string(data))
+		fmt.Println(str)
+	}
+
+	// Output:
+	// test-file.py
+	// [err] unknown filetype: test-file.txt
+	// test-file.sh
+	// test-file.scpt
+	// [err] unknown filetype: test-file.doc
+
+	for _, script := range scripts {
+		if err := os.Remove(script.name); err != nil {
+			panic(err)
+		}
+	}
 }
--- util/util.go
+++ util/util.go
@@ -4,8 +4,42 @@
 // MIT Licence. See http://opensource.org/licenses/MIT
 //
 
-// Package util contains general helper functions for workflow (library)
-// authors.
+/*
+
+Package util contains general helper functions for workflow (library) authors.
+
+The functions can be divided into roughly three groups: paths, formatting
+and scripting.
+
+
+Paths
+
+There are a couple of convenience path functions, MustExist and
+ClearDirectory.
+
+
+Formatting
+
+PrettyPath for user-friendly paths, and the Pad* functions for padding
+strings.
+
+Scripting
+
+QuoteAS quotes strings for insertion into AppleScript code and there
+are several Run* functions for executing script code and files.
+
+	Run()     // run a script file or executable & return output
+	RunAS()   // run AppleScript code & return output
+	RunJS()   // run JXA code & return output
+	RunCmd()  // run *exec.Cmd & return output
+
+Run takes the path to a script or executable. If file is executable,
+it runs the file directly. If it's a script file, it tries to guess the
+appropriate interpreter.
+
+See Runner for more information.
+
+*/
 package util
 
 import (
@@ -38,7 +72,7 @@ func PathExists(path string) bool {
 	panic(err)
 }
 
-// ClearDirectory deletes all files within a directory.
+// ClearDirectory deletes all files within a directory, but not directory itself.
 func ClearDirectory(p string) error {
 	if !PathExists(p) {
 		return nil
